29b618db1e0eccfba8c3e625e936efafd11373c9
cankat-orum <jankatorumanp@gmail.com>
2023-09-28 18:48:27 +0300

    No-gui library app

--- tables01/script.js
+++ tables01/script.js
@@ -1,15 +1,18 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return title + author + pages + read;
-  };
 }
 
-const theHobbit = new Book("The Hobbit", "JRR Tolkien", 295, false);
+function addBook(t, author, pages, read) {
+  const x = new Book(t, author, pages, read);
+  myLibrary.push(x);
+}
 
-console.log(theHobbit.info());
+addBook("test1", "test1", 100, true);
+addBook("test2", "test2", 200, false);
 
-Object.getPrototypeOf(theHobbit) === Book.prototype;
+console.log(myLibrary);
